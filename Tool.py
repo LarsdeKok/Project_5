1186d07cc3a377410b2841b505957c65885fb9eb
Lars de Kok <532151@student.fontys.nl>
2024-09-19 14:12:52 +0200

    oke

--- Tool.py
+++ Tool.py
@@ -2,3 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as st
 import numpy as np
+
+connection_data = pd.read_excel("Connexxion data - 2024-2025.xlsx")
+omloopplanning = pd.read_excel("omloopplanning.xlsx")
