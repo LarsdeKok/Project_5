b739300d94d50d306622e7a303ed0d97afa20668
Lars de Kok <532151@student.fontys.nl>
2024-09-19 15:24:03 +0200

    weg

--- Tests.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import scipy.stats as st
-import numpy as np
-
-data = pd.read_excel("Connexxion data - 2024-2025.xlsx")
-planning = pd.read_excel("omloopplanning.xlsx")
-
-
-print(data.groupby("buslijn"))
