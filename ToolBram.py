0eb119cbf30321b37aa67bdf93a07f68e6921e35
Brimma <bram.bleumink@gmail.com>
2024-09-20 12:56:01 +0200

    _

--- ToolBram.py
+++ ToolBram.py
@@ -1,3 +1,8 @@
-def slpfjeoiasjf():
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import numpy as np
 
-    return
+# Read excel files
+connection_data = pd.read_excel("Connexxion data - 2024-2025.xlsx")
+omloopplanning = pd.read_excel("omloopplanning.xlsx")
