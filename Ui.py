8b6ed59d12ef034e627840e8ffda901fb670b827
Jinne123 <jinnehaan@gmail.com>
2024-09-20 13:32:57 +0200

    Added file

---
+++ Ui.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('test2')
+
+st.write('test')
+
+file = st.file_uploader('Dienstregeling')
+file2 = st.file_uploader('Omloopplanning')
