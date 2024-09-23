953ca33ba9b72ad704735aa42695314372daa051
Jinne123 <jinnehaan@gmail.com>
2024-09-23 10:43:20 +0200

    Made some changes for User interface

--- PlanningChecker.py
+++ PlanningChecker.py
@@ -4,28 +4,14 @@ import numpy as np
 
 from ToolLars import aantal_bussen
 
-
 st.set_page_config(
     page_title="Planning Checker",
     page_icon="👋",
 )
-# st.sidebar.title("Plotting Demo")
+st.title('Planning checker')
+st.write('Made by: Group 8')
 
-# st.sidebar.success("Select a demo above.")
-
-
-st.title('test2')
-
-st.write('test')
-
-
-
-dienstregeling = st.file_uploader('Dienstregeling', type=['xlsx'],accept_multiple_files=False)
-omloop = st.file_uploader('Omloopplanning', type=['xlsx'],accept_multiple_files=False)
-
-inputfields = []
-
-def filesUploaded():
+def filesUploaded(dienstregeling, omloop):
     dfomloop=pd.read_excel(omloop)
     dfdienst=pd.read_excel(dienstregeling)
 
@@ -36,23 +22,37 @@ def filesUploaded():
     generate_values(aantallen)
 
 def generate_values(aantallen):
+    inputfields = []
     for i in aantallen:
         inputfields.append(st.number_input(f"Insert the SOH for bus {i}.", step=1, format="%d", min_value=10, max_value=100))
     if st.button("Submit"):
         st.write('check voor values')
-        doTheChecks()
+        doTheChecks(inputfields)
 
-
-def doTheChecks():
+def doTheChecks(inputfields):
     st.session_state['SOHs'] = inputfields
-    st.switch_page("pages/Results.py")
-    
+    st.session_state['FormFilled'] = True
 
-with st.popover("Submit files"):
-    if dienstregeling is not None and omloop is not None:
-        filesUploaded()
-    else:
-        st.write("Please add the files first.")
+    st.switch_page("pages/Results.py")
+
+if 'FormFilled' not in st.session_state:
+
+
+    dienstregeling = st.file_uploader('Dienstregeling', type=['xlsx'],accept_multiple_files=False)
+    omloop = st.file_uploader('Omloopplanning', type=['xlsx'],accept_multiple_files=False)
+
+    with st.popover("Submit files"):
+        if dienstregeling is not None and omloop is not None:
+            filesUploaded(dienstregeling, omloop)
+        else:
+            st.write("Please add the files first.")
+else:
+    st.write('Do you want to reset your data?')
+    if st.button("Reset"):
+        for key in st.session_state.keys():
+            del st.session_state[key]
+        st.switch_page("PlanningChecker.py")
+
 
 
 
