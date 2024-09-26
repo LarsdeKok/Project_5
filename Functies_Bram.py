839983f42dd94bfb32da2bd84b1745d245aa0a2f
Brimma <bram.bleumink@gmail.com>
2024-09-26 19:02:55 +0200

    _

--- Functies_Bram.py
+++ Functies_Bram.py
@@ -1,1 +1,11 @@
+def omloopplanning_buslijn(omloopplanning):
+    omloopplanning['buslijn'] = omloopplanning['buslijn'].fillna('##')
+    for i in range(len(omloopplanning)):
+        if omloopplanning['activiteit'][i]=='materiaal rit':
+            omloopplanning['buslijn'][i].replace('##','materiaalrit')
+    omloopplanning['buslijn'] = omloopplanning['buslijn'].apply(lambda x: int(x) if isinstance(x, float) else x)
 
+def afstandcode_maken(omloopplanning):
+    omloopplanning['afstandcode']=''
+    for i in range(len(omloopplanning)):
+        omloopplanning['afstandcode'][i]=f"{omloopplanning['startlocatie'][i]}{omloopplanning['eindlocatie'][i]}{omloopplanning['buslijn'][i]}"
