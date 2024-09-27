10b18e7f8ec8f649fe3eea7657caaf5fa083d260
Lars de Kok <532151@student.fontys.nl>
2024-09-27 12:01:51 +0200

    Duur van iedere activiteit in kolom "diff"

--- ToolLars.py
+++ ToolLars.py
@@ -10,4 +10,16 @@ def aantal_bussen(planning):
     bussen = planning[planning.columns[len(planning.columns)-1]].unique()
     return bussen
 
+def duur_activiteiten(omloop):
+    omloop[omloop.columns[3]] = pd.to_datetime(omloop.iloc[:,3], format = "%H:%M:%S")
+    omloop[omloop.columns[4]] = pd.to_datetime(omloop.iloc[:,4], format = "%H:%M:%S")
+    omloop["diff"] = omloop[omloop.columns[4]] - omloop[omloop.columns[3]]
+    return omloop
 
+def oplaadtijd(omloop):
+    oplaadmomenten = omloop[omloop.iloc[:,5].str.contains("opladen")]
+    tekortopladen = oplaadmomenten[oplaadmomenten['diff'] < pd.Timedelta(minutes=15)]
+    tekortopladen.head(10)
+    if len(tekortopladen.index) > 0:
+        tekortopladen
+        print(f"De bovenstaande oplaadmomenten zijn te kort.")
