58b36bab6116cc4b93928fffd9345aa768882f67
Brimma <bram.bleumink@gmail.com>
2024-10-11 18:18:54 +0200

    Eenheid kW aan kolomtitels toegevoegd

--- SOCFloor.py
+++ SOCFloor.py
@@ -4,7 +4,7 @@ import streamlit as st
 def check_SOC(omloopplanning, SOH):
     capaciteit = 300
     SOC_kolom = []
-    omloopplanning['min_batterij'] = 0
+    omloopplanning['min_batterij (kW)'] = 0
 
     for i in range(1, max(omloopplanning['omloop nummer']) + 1):
         df = omloopplanning[omloopplanning['omloop nummer'] == i]
@@ -18,7 +18,7 @@ def check_SOC(omloopplanning, SOH):
 
         batterij_start = 0.9 * max_batterij  # Assume battery is charged to 90%
         min_batterij = 0.1 * max_batterij  # Minimum allowed battery level
-        omloopplanning.loc[omloopplanning['omloop nummer'] == i, 'min_batterij'] = min_batterij
+        omloopplanning.loc[omloopplanning['omloop nummer'] == i, 'min_batterij (kW)'] = min_batterij
 
         # Loop over each row in df
         for idx, row in df.iterrows():
@@ -29,9 +29,9 @@ def check_SOC(omloopplanning, SOH):
 
             SOC_kolom.append(SOC)
         
-    omloopplanning['SOC'] = SOC_kolom
-    omloopplanning['Below_min_SOC'] = omloopplanning['SOC'] < omloopplanning['min_batterij']
-    soc_tolow = omloopplanning[["rijnummer", "startlocatie", "starttijd", "eindtijd", "omloop nummer", "min_batterij","SOC"]][omloopplanning["Below_min_SOC"]==True]
+    omloopplanning['SOC (kW)'] = SOC_kolom
+    omloopplanning['Below_min_SOC'] = omloopplanning['SOC (kW)'] < omloopplanning['min_batterij (kW)']
+    soc_tolow = omloopplanning[["rijnummer", "startlocatie", "starttijd", "eindtijd", "omloop nummer", "min_batterij (kW)","SOC (kW)"]][omloopplanning["Below_min_SOC"]==True]
     if len(soc_tolow) > 0:
         st.write("In the following rows the bus gets below the minimum battery level")
         st.write(soc_tolow)
