42b29422729fce5f3bf7af53e20a0beacb0a114c
Brimma <bram.bleumink@gmail.com>
2024-10-13 18:07:36 +0200

    Output SOC_check verbeterd

--- SOCFloor.py
+++ SOCFloor.py
@@ -1,5 +1,32 @@
 import pandas as pd
 import streamlit as st
+
+def intervals(df:pd.DataFrame)->pd.DataFrame:
+    '''
+    Takes a DataFrame from the omloopplanning and outputs a new DataFrame containing the omloop nummers and the
+    intervals in which they get below their minimum SOC value.
+    '''
+    new_interval=True
+    omloop=[]
+    starttijd=[]
+    eindtijd=[]
+    for i in range(len(df)):
+        if new_interval==True:
+            omloop.append(df['omloop nummer'].iloc[i])
+            starttijd.append(df['starttijd'].iloc[i])
+            new_interval=False
+        elif i not in [0, len(df)-1] and df['starttijd'].iloc[i+1]!=df['eindtijd'].iloc[i]:
+            eindtijd.append(df['eindtijd'].iloc[i])
+            new_interval=True
+        elif i==len(df)-1:
+            eindtijd.append(df['eindtijd'].iloc[i])
+    out=pd.DataFrame()
+    out['omloop nummer']=omloop
+    out['starttijd']=starttijd
+    out['eindtijd']=eindtijd
+    return out
+
+
         
 def check_SOC(omloopplanning, SOH):
     '''
@@ -37,8 +64,12 @@ def check_SOC(omloopplanning, SOH):
     omloopplanning['Below_min_SOC'] = omloopplanning['SOC (kW)'] < omloopplanning['min_batterij (kW)']
     soc_tolow = omloopplanning[["rijnummer", "startlocatie", "starttijd", "eindtijd", "omloop nummer", "min_batterij (kW)","SOC (kW)"]][omloopplanning["Below_min_SOC"]==True]
     if len(soc_tolow) > 0:
-        st.write("In the following rows the bus gets below the minimum battery level")
-        st.write(soc_tolow)
+        st.write("The following busses get below their minimum battery level")
+        st.write(intervals(soc_tolow))
+        
+        #st.write(soc_tolow)
+        #st.write(set(soc_tolow['omloop nummer']))
+        #st.write("In the following rows the bus gets below the minimum battery level")
     else:
         st.write("✓) All busses stay above the minimum battery level")
 
