b991d604c149d95aa752fd0143c9f9294a3f8d34
Lars de Kok <532151@student.fontys.nl>
2024-10-19 21:43:37 +0200

    probeer dingen

---
+++ pages/4-advanced_inputs.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import math as m
+
+from ToolLars import aantal_bussen
+
+def generate_values(aantallen):
+    inputfields = {}
+    for i in aantallen:
+        inputfields[int(i)] = st.number_input(f"Insert the SOH for bus {i}.", step=1, format="%d", min_value=10, max_value=100, value=90)
+
+def mainFunction():
+    omloop = st.session_state["Omloop"]
+    Soh = st.session_state["SOHs"]
+    Dienstregeling = st.session_state["Dienstregeling"]
+    afstandsmatrix = st.session_state["Afstandsmatrix"]
+    aantallen = aantal_bussen(omloop)
+    generate_values(aantallen)
