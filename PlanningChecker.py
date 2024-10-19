2c32545587efcf19107e434cf8c66a8d35128eda
Lars de Kok <532151@student.fontys.nl>
2024-10-19 22:32:06 +0200

    Begin gemaakt aan het krijgen van meer inputvalues\

--- PlanningChecker.py
+++ PlanningChecker.py
@@ -26,10 +26,8 @@ def filesUploaded(dienstregeling, omloop):
 def generate_values(aantallen):
     inputfields = {}
     for i in aantallen:
-        inputfields[int(i)] = st.number_input(f"Insert the SOH for bus {i}.", step=1, format="%d", min_value=10, max_value=100, value=90)
-    if st.button("Submit"):
-        st.write('Check for values')
-        doTheChecks(pd.DataFrame.from_dict(inputfields, orient="index"))
+        inputfields[int(i)] = 90
+    doTheChecks(pd.DataFrame.from_dict(inputfields, orient="index"))
 
 def doTheChecks(inputfields):
     st.session_state['SOHs'] = inputfields
@@ -38,15 +36,26 @@ def doTheChecks(inputfields):
     st.switch_page("pages/2-Results.py")
 
 if 'FormFilled' not in st.session_state:
-    dienstregeling = st.file_uploader('Time table', type=['xlsx'],accept_multiple_files=False)
-    omloop = st.file_uploader('Bus planning', type=['xlsx'],accept_multiple_files=False)
+    # File upload section
+    dienstregeling = st.file_uploader('Time table', type=['xlsx'], accept_multiple_files=False)
+    omloop = st.file_uploader('Bus planning', type=['xlsx'], accept_multiple_files=False)
+    
+    if dienstregeling is not None and omloop is not None:
+        # Ask if the user wants to use advanced options
+        use_advanced = st.checkbox("Use advanced options")
 
-    with st.popover("Submit files"):
-        if dienstregeling is not None and omloop is not None:
-            filesUploaded(dienstregeling, omloop)
-        else:
-            st.write("Please add the files first.")
+        # Submit button for file uploads
+        if st.button("Submit files"):
+            if use_advanced:
+                # Redirect to the advanced options page
+                st.switch_page("pages/1-AdvancedOptions.py")
+            else:
+                # Continue with normal flow if advanced options are not selected
+                filesUploaded(dienstregeling, omloop)
+    else:
+        st.write("Please add the files first.")
 else:
+    # Reset data option
     st.write('Do you want to reset your data?')
     if st.button("Reset"):
         for key in st.session_state.keys():
