60529926a924aca1f07aadd2f62ed686cf65a034
Lars de Kok <532151@student.fontys.nl>
2024-10-31 10:40:03 +0100

    push

--- ToolLars.py
+++ ToolLars.py
@@ -58,7 +58,8 @@ def oplaadtijd(omloop):
         st.error(f"There are {len(tekortopladen)} times a bus is charged too short")
         with st.expander("Click for more information on the charging times mentioned above."):
             st.write("The following charge times need to be longer")
-            st.write(pd.DataFrame(tekortopladen)["starttijd", "eindtijd", "activiteit","omloop nummer", "energieverbruik2"])
+            tekortopladen = tekortopladen["starttijd", "eindtijd", "activiteit","omloop nummer", "energieverbruik2"]
+            st.write(pd.DataFrame(tekortopladen))
     else:
         st.success("✓) All busses get charged sufficiently long.")
 
