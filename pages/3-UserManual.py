b45c2ec3f0ebb4362a1b3c849225549e982fe580
Brimma <bram.bleumink@gmail.com>
2024-10-31 21:43:58 +0100

    User manual

--- pages/3-UserManual.py
+++ pages/3-UserManual.py
@@ -11,19 +11,12 @@ st.title('User manual')
 
 file_path = "Project_5_Manual.pdf"
 
-uploaded_file = st.file_uploader(file_path, type="pdf")
+
 
 def displayPDF(file):
-    base64_pdf = base64.b64encode(file.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
-    st.components.v1.html(pdf_display, height=1000)
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
-
-# def displayPDF(file):
-#     with open(file, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-#     pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
-
-if uploaded_file is not None:
-    displayPDF(uploaded_file)
+displayPDF(file_path)
