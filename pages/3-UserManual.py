394857b76e7ae0d5e5013b3d58d09f754fb8caa0
Lars de Kok <532151@student.fontys.nl>
2024-10-31 22:13:36 +0100

    "Updated iframe height from 1000 to 700 in displayPDF function"

--- pages/3-UserManual.py
+++ pages/3-UserManual.py
@@ -19,7 +19,7 @@ st.download_button(
 )
 
 def displayPDF(url):
-    pdf_display = f'<iframe src="{url}" width="100%" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="{url}" width="100%" height="700" type="application/pdf"></iframe>'
     st.components.v1.html(pdf_display, height=1000)
 
 displayPDF("https://drive.google.com/file/d/1kufRsWI7z0hi0TP6TAXBZRudChxBwCLZ/preview")
