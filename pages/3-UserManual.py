46a788c6614a9c3d21d3f2818abf0931a78cc497
Lars de Kok <532151@student.fontys.nl>
2024-10-31 22:11:49 +0100

    "Moved download button code to top of file, removed duplicate code at bottom"

--- pages/3-UserManual.py
+++ pages/3-UserManual.py
@@ -8,17 +8,6 @@ st.set_page_config(page_title="User manual", page_icon="📖")
 
 st.title('User manual')
 
-
-file_path = "Project_5_Manual.pdf"
-file_link = "https://drive.google.com/file/d/1kufRsWI7z0hi0TP6TAXBZRudChxBwCLZ/view?usp=sharing"
-
-def displayPDF(url):
-    pdf_display = f'<iframe src="{url}" width="100%" height="1000" type="application/pdf"></iframe>'
-    st.components.v1.html(pdf_display, height=1000)
-
-# Use Google Drive's preview link
-displayPDF("https://drive.google.com/file/d/1kufRsWI7z0hi0TP6TAXBZRudChxBwCLZ/preview")
-
 with open("Project_5_Manual.pdf", "rb") as file:
     manual_data = file.read()
 
@@ -29,3 +18,13 @@ st.download_button(
     file_name="PlanningChecker_Manual.pdf",
     mime="application/pdf"
 )
+
+file_path = "Project_5_Manual.pdf"
+file_link = "https://drive.google.com/file/d/1kufRsWI7z0hi0TP6TAXBZRudChxBwCLZ/view?usp=sharing"
+
+def displayPDF(url):
+    pdf_display = f'<iframe src="{url}" width="100%" height="1000" type="application/pdf"></iframe>'
+    st.components.v1.html(pdf_display, height=1000)
+
+# Use Google Drive's preview link
+displayPDF("https://drive.google.com/file/d/1kufRsWI7z0hi0TP6TAXBZRudChxBwCLZ/preview")
