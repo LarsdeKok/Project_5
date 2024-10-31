1273057022f3cf1a011c0bfa36c4d5562d9f8167
Brimma <bram.bleumink@gmail.com>
2024-10-31 21:45:03 +0100

    User manual nu wel gefixt?

--- pages/3-UserManual.py
+++ pages/3-UserManual.py
@@ -11,12 +11,20 @@ st.title('User manual')
 
 file_path = "Project_5_Manual.pdf"
 
-
-
-def displayPDF(file):
-    with open(file, "rb") as f:
+def displayPDF(file_path):
+    with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
+    st.components.v1.html(pdf_display, height=1000)
 
-displayPDF(file_path)
+# Call with the correct file path
+displayPDF(file_path)  # Replace with the PDF file's name
+
+
+# def displayPDF(file):
+#     with open(file, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
+
+# displayPDF(file_path)
